b7ec4ee039b2a86cd41b7674e5956e389675c8ff
Ahmed Yasser <ahmedyasser692004@gmail.com>
2026-07-17 16:58:30 +0300

    making a good project structure and added some codes to allow uploading files

---
+++ src/controllers/DataController.py
@@ -0,0 +1,48 @@
+from fileinput import filename
+import os
+
+from .BaseController import BaseController
+from .ProjectController import ProjectController
+from fastapi import UploadFile
+from models import ResponseSignal
+import re
+
+class DataController(BaseController):
+    
+    def __init__(self):
+        super().__init__()  
+        self.size_scale = 1024 * 1024  # Convert MB to bytes
+        
+    def validate_uploaded_file(self, file: UploadFile):
+        # Validate the file properties
+        
+        if file.content_type not in self.app_settings.FILE_ALLOWED_TYPES :
+            return False, ResponseSignal.FILE_TYPE_NOT_SUPPORTED.value
+
+        if file.size > self.app_settings.FILE_MAX_SIZE * self.size_scale:  # Convert MB to bytes
+            return False, ResponseSignal.FILE_SIZE_EXCEEDED.value
+
+        return True, ResponseSignal.FILE_VALIDATED_SUCCESS.value
+    
+    def generate_unique_filename(self, original_filename: str, project_id: str):
+        
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(project_id=project_id)
+        
+        cleaned_file_name = self.get_clean_filename(orig_file_name=original_filename)
+    
+        new_file_path = os.path.join(project_path, random_key + "_" + cleaned_file_name)
+        
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(project_path, random_key + "_" + cleaned_file_name)
+        
+        return new_file_path
+
+    def get_clean_filename(self, orig_file_name: str):
+        # Remove any unwanted characters from the filename
+        cleaned_file_name = re.sub(r'[^\w.]', '', orig_file_name.strip())
+        
+        cleaned_file_name = cleaned_file_name.replace(" ", "_")
+        
+        return cleaned_file_name
