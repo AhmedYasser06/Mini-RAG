3af1436d5bcfd2d0304c385d13b23f754383e545
Ahmed Yasser <ahmedyasser692004@gmail.com>
2026-07-20 19:44:04 +0300

    File Processing

--- src/models/enums/ResponseEnumeration.py
+++ src/models/enums/ResponseEnumeration.py
@@ -7,4 +7,5 @@ class ResponseSignal(Enum):
     FILE_TYPE_NOT_SUPPORTED = "file_type_not_supported"
     FILE_SIZE_EXCEEDED = "file_size_exceeded"
     FILE_UPLOADED_FAILED = "file_upload_failed"
-    
+    PROCESSING_SUCCESS = "processing_success"
+    PROCESSING_FAILED = "processing_failed"
